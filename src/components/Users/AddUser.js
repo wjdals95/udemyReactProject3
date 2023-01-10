ddd920485f45bb90bd4bc27ac8c824f8b68b982e
wjdals95 <wjdals992@naver.com>
2023-01-10 10:44:29 +0900

    Wrapper 컴포넌트로 감싸기

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import Card from "../UI/Card";
 import Button from "../UI/Button";
 import ErrorModal from "../UI/ErrorModal";
-
+import Wrapper from "../Helpers/Wrapper";
 import classes from "./AddUser.module.css";
 
 const AddUser = (props) => {
@@ -40,7 +40,9 @@ const AddUser = (props) => {
     setError(null);
   };
   return (
-    <div>
+    // div태그로 감싸면 나중엔 div가 많아질수 있고 그러한 이유 때문에 랜더링이 느려질 수 있으므로 Wrapper라는 컴포넌트를 만들어 그 안에는
+    // 인접한 태그들을 부르는 props.children 만 리턴해서 div는 줄이고 리액트 조건은 충족시키며 가벼워지게 만들 수 있다.
+    <Wrapper> 
       {error && (
         <ErrorModal
           title={error.title}
@@ -69,7 +71,7 @@ const AddUser = (props) => {
           <Button type="submit">Add User</Button>
         </form>
       </Card>
-    </div>
+    </Wrapper>
   );
 };
 
